8bcb1015c3a0c3543fb628bf683859a5ea992040
Javed Kathat <javedkhankathat3040@gmail.com>
2024-01-24 23:58:15 +0530

    OS module function

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const os = require('node:os');
 console.log("Platform: " + os.platform());
 console.log("Architecture: " + os.arch());
 
-console.log("Hello world");
+console.log("free memory: " + os.freemem()/(1024*1024));
+console.log("free memory: " + os.totalmem()/(1024*1024*1024));
+console.log("Host Name: " + os.hostname());
+console.log(os.userInfo());
